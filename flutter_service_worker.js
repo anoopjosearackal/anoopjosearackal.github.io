c0e65c667db941cc6c0d6c258691576c15a343ba
Anoop Jose <anoopjosearackal@icloud.com>
2020-08-09 10:22:31 +0100

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,19 +5,23 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "301b8aee0c1b350626eb3f4ee2dd2a16",
 "/": "301b8aee0c1b350626eb3f4ee2dd2a16",
-"main.dart.js": "ed0faf13b62bf141b4235385d475737d",
+"main.dart.js": "fba0ecfc9f7bb6f0cdca14b9a0b9608b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "ba1ce7de7a3c53dffd3f10ed964eaf46",
 ".git/config": "2bfd468a7d3c99be484b4912d7090a82",
 ".git/objects/95/19e1d75e8e60fc461d42dceff7162076484747": "87166efde232eca9c4f1ad4118b6d046",
+".git/objects/57/d33c9b24f69fed9afdc0239d85ba44338d8125": "f582a2037eced9817c4ee8c954807a91",
 ".git/objects/03/eaddffb9c0e55fb7b5f9b378d9134d8d75dd37": "87850ce0a3dd72f458581004b58ac0d6",
+".git/objects/9c/2a605b22a994506ce9e49fa8c2692b8d08657b": "ea92724ef7848f16c2d70c90357c8153",
 ".git/objects/02/e6ad2eccb1ac721500151f7e22b9a6a8ee576f": "da13abd73468ed766146da15125bd564",
 ".git/objects/a3/7e84939bc29f2259ed23c9cf0e802f6ba295a3": "38ee90ed0b14c7caf6f356e610c660f2",
 ".git/objects/d0/a5912947ceb27505c2661b95b18352688a4b73": "5c7526ce1c2c2423ec7c6459cc90b530",
+".git/objects/b4/b5b236968d1ae737a56ca8689e3b8d022cec04": "5353532d1b0b98576cfd1503511a807a",
 ".git/objects/ae/96eb75128fc6775709cdca9ac814369214ad83": "ad71d4ebbd0e1db44611b043b21a6984",
 ".git/objects/fc/565ec170b7fd55cbf422b8188a6f64eddda60e": "c1d5b622c8e63d1865785872c9d4a21d",
+".git/objects/fb/e5ccf3ee9b2b15141f7db67b74f139cf364c3d": "468aaa629816d5fa954078ac8e358313",
 ".git/objects/ec/8b7448ae7e02442e905fd5414fc35ce8deb5f4": "f8dafcf6a9d46bda640d8dc3c8a7e0b0",
 ".git/objects/20/5bb5db271c6d8de8399864c7bb9b917f638893": "c993b22f115d7f3ae6d5b7b212806539",
 ".git/objects/8a/aa46ac1ae21512746f852a42ba87e4165dfdd1": "1d8820d345e38b30de033aa4b5a23e7b",
@@ -28,17 +32,21 @@ const RESOURCES = {
 ".git/objects/54/35315e0891c6528e3d281c3c8ddae122b52d78": "b69f569ee3e03857209e078d9d29d297",
 ".git/objects/6d/43f5fd3f7089044528f15821723db6cf5cc3ea": "a3c710214a69dba3d575b3faa5ac0f10",
 ".git/objects/0f/816fb5068fb5d0dc1623718a94d7a34c5edfe4": "48392ce692d6328aef69a753fa305233",
+".git/objects/bf/65e7ecaec992e585996c3ceb42829a1baea78a": "514e1aa93cb3a5b0b6cddbd8a919ccbd",
 ".git/objects/bf/b823b3cb3860a4861f03bdc0610bd68e9212c7": "feb1398a076873007f9ac93cc97a9c8f",
 ".git/objects/dd/56eabbf298449db91bfa8644c6fbb20b8e6dda": "3fa6bb294070401dbba0b01792b84ce0",
 ".git/objects/b7/49bfef07473333cf1dd31e9eed89862a5d52aa": "36b4020dca303986cad10924774fb5dc",
 ".git/objects/db/e528d1a32ca92ec1b326ee56773891824086ed": "cc768b6278610ebb3adacf56c9619a4d",
+".git/objects/cc/e1b7b77e6fdfb9ad99690378328bb630ac0b5f": "95304c502e57a5759f0f782abf2c0876",
+".git/objects/1b/cfac9a07dafb8c8b5c0f051425a1b61993ee65": "0862f99c16cd46da1fdc96b4d0bd7e40",
 ".git/objects/15/9b15011009e1e691b0761e5627dc3f3aa452ad": "822c0ced8e90203ed87ff7e0082d7945",
+".git/objects/1d/97d7898243b26055fb57171900f7e6ac86a603": "0654d03a5b74fcf48d4568d2ec65c0a8",
 ".git/objects/13/cd698afa86edb334bec59991cd113491a5a4f1": "01d69635e49925db17e7c32e26fc974f",
 ".git/HEAD": "4cf2d64e44205fe628ddd534e1151b58",
 ".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
-".git/logs/HEAD": "809daf44d24ebd8b4654b61e003b87cc",
-".git/logs/refs/heads/master": "809daf44d24ebd8b4654b61e003b87cc",
-".git/logs/refs/remotes/origin/master": "0392293fffac74ed2d42f662f7a2f09d",
+".git/logs/HEAD": "0b8b7c957bfa9149916642feb9a22538",
+".git/logs/refs/heads/master": "0b8b7c957bfa9149916642feb9a22538",
+".git/logs/refs/remotes/origin/master": "9012e0dcdcba0602fb16400303eeba5a",
 ".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
 ".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
 ".git/hooks/pre-rebase.sample": "56e45f2bcbc8226d2b4200f7c46371bf",
@@ -52,16 +60,17 @@ const RESOURCES = {
 ".git/hooks/pre-applypatch.sample": "054f9ffb8bfe04a599751cc757226dda",
 ".git/hooks/pre-push.sample": "3c5989301dd4b949dfa1f43738a22819",
 ".git/hooks/update.sample": "517f14b9239689dff8bda3022ebd9004",
-".git/refs/heads/master": "c2d7bf36d0d43e616734d01f6e8df0e1",
-".git/refs/remotes/origin/master": "c2d7bf36d0d43e616734d01f6e8df0e1",
-".git/index": "141b6bfa673ec0853f514dc657d3baaa",
-".git/COMMIT_EDITMSG": "a8297d555dd34879e8e48e1cf12acefa",
+".git/refs/heads/master": "3269ff6fe39bc848a7add761fef48af8",
+".git/refs/remotes/origin/master": "3269ff6fe39bc848a7add761fef48af8",
+".git/index": "8aabd58eafdac3b0f133f6ad488a7bb9",
+".git/COMMIT_EDITMSG": "0e76b636917f10be0d6f4432de4fc29b",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "fc068aa3838854819873364eeea79982",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/NOTICES": "9a446cb58c7071f65807f71218fd8f7a",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/anoop.jpeg": "69defd99fa4436fa90b17a88824953bd",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -78,8 +87,8 @@ const CORE = [
 self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
-      // Provide a no-cache param to ensure the latest version is downloaded.
-      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'no-cache'})));
+      // Provide a 'reload' param to ensure the latest version is downloaded.
+      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -162,7 +171,7 @@ self.addEventListener("fetch", (event) => {
         // Either respond with the cached resource, or perform a fetch and
         // lazily populate the cache. Ensure the resources are not cached
         // by the browser for longer than the service worker expects.
-        var modifiedRequest = new Request(event.request, {'cache': 'no-cache'});
+        var modifiedRequest = new Request(event.request, {'cache': 'reload'});
         return response || fetch(modifiedRequest).then((response) => {
           cache.put(event.request, response.clone());
           return response;
